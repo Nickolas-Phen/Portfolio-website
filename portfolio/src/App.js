68f51a21a9685fcc97e39ef2aec008e4ac45d51e
Nickolas_Phen <nickolas.phen@gmail.com>
2021-08-23 22:31:48 -0400

    Basic wireframe

--- portfolio/src/App.js
+++ portfolio/src/App.js
@@ -1,24 +1,25 @@
-import logo from './logo.svg';
+import React from 'react'
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <React.Fragment>
+      <body className="Center-header">
+        <h1> I am Nickolas Phen,</h1>
+        <h2>Welcome to my website</h2>
+      </body>
+      <body className="about-grid">
+        <h1 className="item-a">About</h1>
+        <h1 className="item-s">Skills</h1>
+      </body>
+      <body className="test2">
+        <h1>Projects</h1>
+      </body>
+      <body className="test">
+        <h1>Contact</h1>
+      </body>
+    </React.Fragment>
+    
   );
 }
 
