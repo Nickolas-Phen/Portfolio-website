d531f525880fee09a449a3b4616ba0d97a3457f0
Nickolas_Phen <nickolas.phen@gmail.com>
2021-08-27 00:10:22 -0400

    Implemented about,skills, and projects

--- portfolio/src/App.js
+++ portfolio/src/App.js
@@ -1,7 +1,11 @@
 import React from 'react'
 import './App.css';
+import images from './images/index.js';
+
+
 
 function App() {
+  
   return (
     <React.Fragment>
       <body className="Center-header">
@@ -9,13 +13,37 @@ function App() {
         <h2>Welcome to my website</h2>
       </body>
       <body className="about-grid">
-        <h1 className="item-a">About</h1>
-        <h1 className="item-s">Skills</h1>
+        <body className="item-a">
+          <h1>About</h1>
+          <p>Hi, my name is Nickolas Phen and I am a senior at the University of Florida working towards a Bachelor's of Science with Computer Science as my major and Electrical Engineering as my minor.
+          
+          </p>
+        </body>
+        <body className="item-s">
+          <h1>Skills</h1>
+          <h2>Proficient with:</h2>
+            <img src={images.java} alt="Java" width="80" height="80"/>
+            <img src={images.python} alt="Python" width="80" height="80" />
+            <img src={images.cpp} alt="C++" width="80" height="80"/>
+            <img src={images.sql} alt="SQL" width="80" height="80"/>
+          <h2>Familiar with:</h2>
+            <img src={images.matlab} alt="Matlab" width="80" height="80"/>
+            <img src={images.react} alt="React" width="80" height="80" />
+            <img src={images.html} alt="HTML" width="80" height="80"/>
+            <img src={images.css} alt="CSS" width="80" height="80"/>
+            <img src={images.vhdl} alt="VHDL" width="80" height="80"/>
+            <img src={images.github} alt="SQL" width="80" height="80"/>
+        </body>
       </body>
-      <body className="test2">
-        <h1>Projects</h1>
+      <h1>Projects</h1>
+      <body className="project-grid">
+        <img src={images.hw} alt="Heavenly Writing" width="800" height="auto"></img>
+        <p>Heavenly writing</p>
+        <p className="projectA">Trail Tracker</p>
+        <img className="projectB" src={images.tt} alt="TrailTracker" width="800" height="auto"></img>
+        
       </body>
-      <body className="test">
+      <body >
         <h1>Contact</h1>
       </body>
     </React.Fragment>
---
+++ portfolio/src/images/index.js
@@ -0,0 +1,17 @@
+const images={
+    cpp: require('./C++.png').default,
+    css: require('./Css.png').default,
+    html: require('./Html5.png').default,
+    java: require('./Java.png').default,
+    js: require('./Javascript.png').default,
+    matlab: require('./Matlab.svg').default,
+    python: require('./Python.png').default,
+    react: require('./React.png').default,
+    sql: require('./Sql.png').default,
+    vhdl: require('./Vhdl.png').default,
+    github: require('./Github.png').default,
+    hw: require('./HeavenlyWriting.png').default,
+    tt: require('./Trailtracker.png').default,
+}
+
+export default images;
